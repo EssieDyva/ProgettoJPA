c2913ef144f233c1ea08ceb8e79fc5a96999fa34
EssieDyva <102621809+EssieDyva@users.noreply.github.com>
2026-02-23 17:36:35 +0100

    fixed wrong import

--- src/main/java/com/betacom/jpa/controllers/AbbonamentoController.java
+++ src/main/java/com/betacom/jpa/controllers/AbbonamentoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,6 @@ import com.betacom.jpa.dto.inputs.AbbonamentoRequest;
 import com.betacom.jpa.response.Resp;
 import com.betacom.jpa.services.interfaces.IAbbonamentoServices;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
